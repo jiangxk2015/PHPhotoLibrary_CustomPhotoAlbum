a3e7c370cc8c78df1de3df5319a34251861f2b25
jiangxk <jiangxk@jiangxkdeiMac.local>
2016-03-04 10:27:30 +0800

    first commit

--- PHAsset_CustomPhotoAlbum/ViewController.m
+++ PHAsset_CustomPhotoAlbum/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "PHPhotoLibrary+CustomPhotoAlbum.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //    [[PHPhotoLibrary sharedPhotoLibrary] createNewAlbumCalled:@"ios9新相册"];
+#if 1
+    [[PHPhotoLibrary sharedPhotoLibrary] saveImage:[UIImage imageNamed:@"猫猫.jpg"] ToAlbum:@"新相册" completion:^(PHAsset *imageAsset) {
+        NSLog(@"%@",imageAsset);
+    } failure:^(NSError *error) {
+    }];
+#endif
+    
+#if 0
+    [[PHPhotoLibrary sharedPhotoLibrary] saveImageWithImageUrl:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"猫猫" ofType:@"jpg"]] ToAlbum:@"ios9新相册" completion:^(PHAsset *imageAsset) {
+        NSLog(@"%@",imageAsset);
+    } failure:^(NSError *error) {
+        
+    }];
+#endif
+    
+#if 0
+    [[PHPhotoLibrary sharedPhotoLibrary] saveImageData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"猫猫" ofType:@"jpg"]] ToAlbum:@"ios9新相册1" completion:^(PHAsset *imageAsset){
+        NSLog(@"%@",imageAsset);
+    } failure:^(NSError *error) {
+        
+    }];
+#endif
+    
+    
+#if 0
+    [[PHPhotoLibrary sharedPhotoLibrary] saveVideoWithUrl:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"bug" ofType:@"mov"]] ToAlbum:@"视频相册2" completion:^(NSURL *videoUrl) {
+        
+    } failure:^(NSError *error) {
+        
+    }];
+#endif
+    
+    
+#if 0
+    [[PHPhotoLibrary sharedPhotoLibrary] loadImagesFromAlbum:@"新相册" completion:^(NSMutableArray *images, NSError *error) {
+        NSLog(@"%@",images);
+    }];
+#endif
+    
 }
 
 - (void)didReceiveMemoryWarning {
